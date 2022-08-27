2732a8941fbf3c66543005c8aba27e9161380d12
mardiana1 <mardiana@code.id>
2022-08-27 13:35:37 +0700

    init

---
+++ config/auth_db.js
@@ -0,0 +1,5 @@
+const AUTH_DB_MYSQL = 'postgres://postgres:mekarsari@localhost:5432/DbTest'
+
+export{
+    AUTH_DB_MYSQL
+}
---
+++ docker-compose/init-db.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+phone_number VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+bio TEXT,
+location VARCHAR(255),
+status boolean,
+verified boolean,
+verified_at TIMESTAMP,
+reset_password_token VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP,
+deleted_at TIMESTAMP
+) 
+
+
+CREATE TABLE roles (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+is_active BOOLEAN,
+created_at TIMESTAMP,
+updated_at TIMESTAMP
+) 
+
+CREATE TABLE role_user (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+role_id INT
+) 
+
+CREATE TABLE permissions (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+description TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP
+) 
+
+CREATE TABLE permission_role (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+role_id INT,
+permission_id INT,
+status BOOLEAN ,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP
+) 
+
+
+
+
