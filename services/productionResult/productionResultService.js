8bdc6207070f381c53260012aa24f234d2ad714f
mardiana1 <mardiana@code.id>
2022-09-25 11:25:15 +0700

    update prod res

--- services/productionResult/productionResultService.js
+++ services/productionResult/productionResultService.js
@@ -1,6 +1,6 @@
 const serviceResponse = require('../../models/responses/serviceResponse')
 const productionResultRepository = require('../../repositories/productionResultRepository')
-const {getProductionResultArrObj} = require('../../models/objects/productionResult')
+const {getProductionResultArrObj, getProductionResultObj} = require('../../models/objects/productionResult')
 const {buildCondition,fetchSortBy} = require('../../repositories/conditionBuilder/knexConditionBuilder')
 
 const addProductionResult = async(productionResult)=>{
@@ -37,9 +37,30 @@ const getDetailMachineProgres = async(paramsQuery)=>{
         var order = await fetchSortBy(paramsQuery)
 
         var prodRes = await productionResultRepository.findAll(paramsBuilder,order)
+
+        prodRes.content = await validatePercentageResult(prodRes.content);
+
         return prodRes
     } catch (error) {
         return serviceResponse(500,error.message)
+    }
+}
+
+const validatePercentageResult = async(content)=>{
+    try {
+        var newContent = []
+        for(let prodRes of content){
+            var newProdRes = getProductionResultObj(prodRes)
+            newProdRes.oee = Number(newProdRes.oee).toFixed(2)
+            newProdRes.availability = Number(newProdRes.availability).toFixed(2)
+            newProdRes.performance = Number(newProdRes.performance).toFixed(2)
+            newProdRes.quality = Number(newProdRes.quality).toFixed(2)
+            newProdRes.achievement = Number(newProdRes.achievement).toFixed(2)
+            newContent.push(newProdRes)
+        }
+        return newContent
+    } catch (error) {
+        throw new Error(error.message)   
     }
 }
 
